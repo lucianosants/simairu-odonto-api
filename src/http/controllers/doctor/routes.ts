a50c995f0db4dd106cad5706486d3d211e895a74
lucianosants <lucianosantos.developer@gmail.com>
2024-03-11 09:58:41 -0300

    feat(middlewares): implements jwt verification

--- src/http/controllers/doctor/routes.ts
+++ src/http/controllers/doctor/routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
 
 import { RegisterDoctor } from './register-doctor';
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
 
 export async function doctorRoutes(app: FastifyInstance) {
-	app.post('/doctors', RegisterDoctor);
+	app.post('/doctors', { onRequest: verifyJWT }, RegisterDoctor);
 }
