bb1a0a652573bad69fbfcde67e00083ce323ad75
lucianosants <lucianosantos.developer@gmail.com>
2024-03-25 09:26:16 -0300

    feat(appointment): implements route for get by id

---
+++ src/http/controllers/appointments/get-appointment-by-id.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { makeGetAppointmentByIdUseCase } from '@/use-cases/factories/make-get-appointment-by-id-use-case';
+import { AppointmentNotFoundError } from '@/use-cases/errors/appointment-not-found-error';
+
+export async function getAppointmentById(
+	request: FastifyRequest,
+	reply: FastifyReply
+) {
+	const getAppointmentByIdParamsSchema = z.object({
+		id: z.string(),
+	});
+
+	const { id } = getAppointmentByIdParamsSchema.parse(request.params);
+
+	try {
+		const getAppointmentByIdUseCase = makeGetAppointmentByIdUseCase();
+
+		const appointment = await getAppointmentByIdUseCase.execute({ id });
+
+		return reply.status(200).send(appointment);
+	} catch (error) {
+		if (error instanceof AppointmentNotFoundError) {
+			return reply.status(400).send({ message: error.message });
+		}
+
+		throw error;
+	}
+}
--- src/http/controllers/appointments/routes.ts
+++ src/http/controllers/appointments/routes.ts
@@ -4,8 +4,10 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt';
 
 import { createAppointment } from './create-appointment';
 import { findAllAppointments } from './find-all-appointments';
+import { getAppointmentById } from './get-appointment-by-id';
 
 export async function appointmentsRoutes(app: FastifyInstance) {
 	app.post('/appointments', { onRequest: verifyJWT }, createAppointment);
 	app.get('/appointments', { onRequest: verifyJWT }, findAllAppointments);
+	app.get('/appointments/:id', { onRequest: verifyJWT }, getAppointmentById);
 }
